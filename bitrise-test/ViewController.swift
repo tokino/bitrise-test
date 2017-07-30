5c2c0370b99b57725d6bf7e1a9093a11b444246a
tokino <t.kino0219@gmail.com>
2017-07-30 12:49:05 +0900

    Initial Commit

---
+++ bitrise-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitrise-test
+//
+//  Created by Tomohiro Kino on 2017/07/30.
+//  Copyright © 2017年 Tomohiro Kino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bitrise-testTests/bitrise_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  bitrise_testTests.swift
+//  bitrise-testTests
+//
+//  Created by Tomohiro Kino on 2017/07/30.
+//  Copyright © 2017年 Tomohiro Kino. All rights reserved.
+//
+
+import XCTest
+@testable import bitrise_test
+
+class bitrise_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
